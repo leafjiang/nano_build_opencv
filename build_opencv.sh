16245de889d91cab89c9cd9dc079e6294c3f2ea8
Leaf Jiang <leaf@nodarsensor.com>
2020-10-08 23:25:49 -0400

    Fix opencv installation for python 3.7

--- build_opencv.sh
+++ build_opencv.sh
@@ -106,6 +106,12 @@ configure () {
         -D BUILD_EXAMPLES=OFF
         -D BUILD_opencv_python2=ON
         -D BUILD_opencv_python3=ON
+        -D PYTHON3_EXECUTABLE=$(which python3.7)
+        -D PYTHON_INCLUDE_DIR=$(python3.7 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
+        -D PYTHON_INCLUDE_DIR2=$(python3.7 -c "from os.path import dirname; from distutils.sysconfig import get_config_h_filename; print(dirname(get_config_h_filename()))")
+        -D PYTHON_LIBRARY=$(python3.7 -c "from distutils.sysconfig import get_config_var;from os.path import dirname,join ; print(join(dirname(get_config_var('LIBPC')),get_config_var('LDLIBRARY')))")
+        -D PYTHON3_NUMPY_INCLUDE_DIRS=$(python3.7 -c "import numpy; print(numpy.get_include())")
+        -D PYTHON3_PACKAGES_PATH=$(python3.7 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") 
         -D CMAKE_BUILD_TYPE=RELEASE
         -D CMAKE_INSTALL_PREFIX=${PREFIX}
         -D CUDA_ARCH_BIN=5.3,6.2,7.2
